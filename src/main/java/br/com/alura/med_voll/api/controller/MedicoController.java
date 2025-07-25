cd556e90c998527b6f11e4b21de2d1a928968cba
Alef <alef.programer@gmail.com>
2025-07-25 16:29:44 -0300

    Implementado novo metodo e dto para fazer o return dos dados para a listagem dos medicos.

--- src/main/java/br/com/alura/med_voll/api/controller/MedicoController.java
+++ src/main/java/br/com/alura/med_voll/api/controller/MedicoController.java
@@ -1,12 +1,15 @@
 package br.com.alura.med_voll.api.controller;
 
 import br.com.alura.med_voll.api.medico.DadosCadastroMedico;
+import br.com.alura.med_voll.api.medico.DadosListagemMedico;
 import br.com.alura.med_voll.api.medico.Medico;
 import br.com.alura.med_voll.api.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -21,6 +24,12 @@ public class MedicoController {
     @Transactional // Utilizado pois é um metodo de insert, em que dados vao ser inseridos
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
         repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public List<DadosListagemMedico> listar(){//Criar dto para devolver dados
+        return repository.findAll().stream()
+                .map(DadosListagemMedico::new ).toList();//É preciso converter de Medico para DadosListagemMedico
     }
 
 }
