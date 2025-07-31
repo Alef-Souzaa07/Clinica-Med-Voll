5e2d77acc274c5dbbef0978311a88b9606e73cc2
Alef <alef.programer@gmail.com>
2025-07-31 13:18:22 -0300

    Implementado metodo que utiliza o token para efetuar o login

--- src/main/java/br/com/alura/med_voll/api/controller/AutenticacaoController.java
+++ src/main/java/br/com/alura/med_voll/api/controller/AutenticacaoController.java
@@ -2,7 +2,10 @@ package br.com.alura.med_voll.api.controller;
 
 import br.com.alura.med_voll.api.domain.usuario.DadosAutenticacao;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +15,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class AutenticacaoController {
 
+    @Autowired
+    private AuthenticationManager manager;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
-
-
+        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(token);
+        return ResponseEntity.ok().build();
     }
 
 }
